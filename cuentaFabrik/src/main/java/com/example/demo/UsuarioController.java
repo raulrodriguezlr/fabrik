8ea44b2ac09e1d838206259b7069c6b227260cec
raulrodriguezlr <12rulo12@gmail.com>
2022-03-30 20:50:59 +0200

    added proyect

---
+++ cuentaFabrik/src/main/java/com/example/demo/UsuarioController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+public class UsuarioController {
+
+	@GetMapping("/")
+	public String lists(Model model) {
+		return "index";
+	}
+}
